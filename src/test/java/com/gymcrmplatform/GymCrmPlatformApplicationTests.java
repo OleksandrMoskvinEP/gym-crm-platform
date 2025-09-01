918386d60c69155c1ea0425182bc2a5b5a4ad518
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-09-01 10:08:57 +0300

    Initial commit

---
+++ src/test/java/com/gymcrmplatform/GymCrmPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymcrmplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
