166603c399833c8ffbc5d516cea87926f65de51d
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-09-05 12:04:52 +0300

    GCA-167 Create controller with swagger annotations

--- trainers-workload-service/src/main/java/com/trainersworkloadservice/model/dto/WorkloadEventRequest.java
+++ trainers-workload-service/src/main/java/com/trainersworkloadservice/model/dto/WorkloadEventRequest.java
@@ -11,7 +11,7 @@ public record WorkloadEventRequest(
         @NotBlank String username,
         @NotBlank String firstName,
         @NotBlank String lastName,
-        @NotBlank Boolean isActive,
+        @NotNull Boolean isActive,
         @NotNull @JsonFormat(pattern = "yyyy-MM-dd") LocalDate trainingDate,
         @Positive Long trainingDuration,
         @NotBlank String actionType
