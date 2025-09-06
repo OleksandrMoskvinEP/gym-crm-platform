f771bd87f792b1238af44fbd0d4002cb78687acf
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-09-06 11:19:31 +0300

    GCA-169 Implement discovery service

--- gym-crm-core/src/main/java/com/gym/crm/app/GymCrmCoreApplication.java
+++ gym-crm-core/src/main/java/com/gym/crm/app/GymCrmCoreApplication.java
@@ -2,7 +2,9 @@ package com.gym.crm.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GymCrmCoreApplication {
     public static void main(String[] args) {
