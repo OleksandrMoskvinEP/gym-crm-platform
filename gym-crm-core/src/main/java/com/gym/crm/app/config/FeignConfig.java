4ee9c9c838072e34a5acd1585abb56f7b94fdac0
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-09-09 11:13:55 +0300

    GCA-172 Create CrossServiceJwtTokenProvider

---
+++ gym-crm-core/src/main/java/com/gym/crm/app/config/FeignConfig.java
@@ -0,0 +1,19 @@
+package com.gym.crm.app.config;
+
+import com.gym.crm.app.security.jwt.CrossServiceJwtTokenProvider;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FeignConfig implements RequestInterceptor {
+    private final CrossServiceJwtTokenProvider crossServiceJwtTokenProvider;
+
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header("Authorization", "Bearer " + crossServiceJwtTokenProvider.generateTokenForService());
+    }
+}
