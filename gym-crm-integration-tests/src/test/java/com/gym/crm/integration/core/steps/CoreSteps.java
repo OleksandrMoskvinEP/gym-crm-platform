2da725710c5ad2e12cf3c86c2f0f69f2e02d5ac9
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-10-04 14:20:56 +0300

    GCA-204 create empty tests

--- gym-crm-integration-tests/src/test/java/com/gym/crm/integration/core/steps/CoreSteps.java
+++ gym-crm-integration-tests/src/test/java/com/gym/crm/integration/core/steps/CoreSteps.java
@@ -1,11 +1,65 @@
 package com.gym.crm.integration.core.steps;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CoreSteps {
-    @Given("core service is running")
-    public void core_service_is_running() {
+    @Given("a user {string} with password {string} exists")
+    public void a_user_with_password_exists(String string, String string2) {
     }
+    @When("I authenticate with username {string} and password {string}")
+    public void i_authenticate_with_username_and_password(String string, String string2) {
+        //отправить post запрос на localhost:8081/api/core/v1/login
+        //с телом запроса {
+        // "username": "arnold.schwarzenegger",
+        //  "password": "qwerty1234"
+        //  }
+    }
+    @Then("I receive a valid JWT token")
+    public void i_receive_a_valid_jwt_token() {
+        //проверить что в ответе есть поле token и оно не пустое
+        //сохранить токен в переменную
+    }
+    @When("I attempt to authenticate with wrong password {string}")
+    public void iAttemptToAuthenticateWithWrongPassword(String arg0) {
+        //отправить post запрос на localhost:8081/api/core/v1/login
+        //с телом запроса {
+        // "username": "arnold.schwarzenegger",
+        //  "password": "qwerty"
+        //  }
+    }
+    @Then("I should receive a response with status code {int}")
+    public void i_should_receive_a_response_with_status_code(Integer int1) {
+    }
+    @Then("the error message should contain {string}")
+    public void the_error_message_should_contain(String string) {
+    }
+
+    @Given("a trainee with username {string} exists")
+    public void a_trainee_with_username_exists(String string) {
+    }
+    @When("I request the trainee profile by username {string}")
+    public void i_request_the_trainee_profile_by_username(String string) {
+        //отправить get запрос на localhost:8081/api/core/v1/trainees/{username}
+        //в заголовке Authorization должен быть токен полученный при аутентификации
+    }
+    @Then("I should retrieve a trainee profile with first name {string} and last name {string}")
+    public void i_should_retrieve_a_trainee_profile_with_first_name_and_last_name(String string, String string2) {
+   //проверить что в ответе есть поля firstName и lastName и они равны ожидаемым значениям
+    }
+
+    @When("I request the list of training types")
+    public void i_request_the_list_of_training_types() {
+    //отправить get запрос на localhost:8081/api/core/v1/training-types
+    //в заголовке Authorization должен быть токен полученный при аутентификации
+    }
+    @Then("I should receive {int} training types")
+    public void i_should_receive_training_types(Integer int1) {
+    //проверить что в ответе массив с типами тренировок и его размер равен int1
+    }
+
+
 }
--- gym-crm-integration-tests/src/test/resources/features/core/core.feature
+++ gym-crm-integration-tests/src/test/resources/features/core/core.feature
@@ -1,5 +1,29 @@
 @core
-Feature: Core service management
 
-  Scenario: Context loads
-    Given core service is running
+Feature: Trainee,Trainer and Trainings Management
+
+  @PositiveCase
+  Scenario: Successful authentication of an existing user
+    Given a user "arnold.schwarzenegger" with password "qwerty1234" exists
+    When I authenticate with username "arnold.schwarzenegger" and password "qwerty1234"
+    Then I receive a valid JWT token
+
+  @NegativeCase
+  Scenario: Failed authentication with incorrect password
+    When I attempt to authenticate with wrong password "wrongpassword"
+    Then I should receive a response with status code 400
+    And the error message should contain "Invalid username or password"
+
+  Scenario: Find trainee profile by valid username
+    Given a trainee with username "olga.ivanova" exists
+    When I request the trainee profile by username "olga.ivanova"
+    Then I should retrieve a trainee profile with first name "Olga" and last name "Ivanova"
+
+  Scenario: Get available training types
+    When I request the list of training types
+    Then I should receive 6 training types
+
+
+
+
+
