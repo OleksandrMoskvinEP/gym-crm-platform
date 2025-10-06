1ee4081ecbb1900b2c389b68828ce589a3318260
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-10-06 15:46:40 +0300

    GCA-204 create multi feature config

---
+++ gym-crm-integration-tests/src/test/java/com/gym/crm/integration/crosservice/steps/Check3Steps.java
@@ -0,0 +1,31 @@
+package com.gym.crm.integration.crosservice.steps;
+
+import com.gym.crm.integration.crosservice.CrosserviceTestConfiguration;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = CrosserviceTestConfiguration.class)
+public class Check3Steps {
+    private int a, b, c, result;
+
+    @Given("I have numbers {int} and {int} and {int}")
+    public void i_have_numbers_and_and(Integer int1, Integer int2, Integer int3) {
+        this.a = int1;
+        this.b = int2;
+        this.c = int3;
+    }
+
+    @When("I operate with them")
+    public void i_operate_with_them() {
+        result = (a + b) / c;
+    }
+
+    @Then("the result should be grater than {int}")
+    public void the_result_should_be_grater_than(Integer int1) {
+        if (result <= int1) {
+            throw new AssertionError("The result should be grater than " + int1);
+        }
+    }
+}
---
+++ gym-crm-integration-tests/src/test/resources/features/crosservice/check3.feature
@@ -0,0 +1,8 @@
+Feature: Basic Cucumber check
+  This feature exists only to verify that Cucumber and Spring context start correctly.
+
+  Feature: Math operations
+  Scenario: Checking math skills
+    Given I have numbers 10 and 20 and 30
+    When I operate with them
+    Then the result should be grater than 0
